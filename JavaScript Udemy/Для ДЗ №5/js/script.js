20d8a99e9b6cbb137b4af03b7c75141cb29df160
Valdemar <kalyuzhnyy.volodia@gmail.com>
2019-11-19 15:39:56 +0200

    hm-5-6

--- JavaScript Udemy/Для ДЗ №5/js/script.js
+++ JavaScript Udemy/Для ДЗ №5/js/script.js
@@ -1,16 +1,24 @@
-let menuItem = document.querySelectorAll('.menu-item'),
-    menu = document.querySelector('.menu'),
-    liLast = document.createElement('li');
+let menu = document.getElementsByClassName("menu")[0],
+    menuItem = document.getElementsByClassName("menu-item"),
+    title = document.getElementById("title"),
+    adv = document.getElementsByClassName("adv")[0],
+    promptforApple = document.querySelector("#prompt"),
+    menuItemLi = document.createElement("li");
 
-liLast.innerHTML = 'Пятый пункт';
-liLast.classList.add('menu-item');
-menu.append(liLast);
+menu.insertBefore(menuItem[2], menuItem[1]);                    // Поменяли местами два элемента
+
+menuItemLi.classList.add("menu-item");                          // Добавляем новый li, с классом и текстом
+menuItemLi.textContent = "Пятый элемент";                       
+menu.appendChild(menuItemLi);                                   
 
 
-menu.insertBefore(menuItem[2], menuItem[1]);
+document.body.style.backgroundImage = "url('img/apple_true.jpg')";  // Меняем фон
 
 
+title.textContent = "Мы продаем только подлинную технику Apple";    // Заменить текст
 
+adv.remove();                                                   // Удалить рекламу со страницы
 
-
+let yourOpinion = prompt("Ваше отношение к технике Apple?");   // Отношение к технике Apple
+promptforApple.textContent = yourOpinion;
 
