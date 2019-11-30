b2367343669b516920e2e03739b6b31a480ce5c4
Valdemar-DEV <kalyuzhnyy.volodia@gmail.com>
2019-11-30 16:24:47 +0200

    timer

--- JavaScript Udemy/yoga/js/script.js
+++ JavaScript Udemy/yoga/js/script.js
@@ -1,6 +1,6 @@
 window.addEventListener('DOMContentLoaded', function () {
     'use strict';
-
+    //Tabs
     let tab = document.querySelectorAll('.info-header-tab'),
         info = document.querySelector('.info-header'),                      //Назначаємо змінні
         tabContent = document.querySelectorAll('.info-tabcontent');
@@ -36,5 +36,60 @@ window.addEventListener('DOMContentLoaded', function () {
             }
         }
     });
-});
 
+    //timer
+
+    let deadline = '2019-12-01';
+
+    function getTimeRemaining (endtime) {
+        let t = Date.parse(endtime) - Date.parse(new Date()),
+            seconds = Math.floor((t/ 1000) % 60),   // отримуєм секунди
+            minutes = Math.floor((t / 1000 / 60) % 60), // отримуєм хвилини
+            hours = Math.floor((t / (1000 * 60 * 60)));   // отримуєм години
+            // days1 = Math.floor((t / 1000 / 60 /60) % 24),           Спосіб отримати дні
+            // days2 = Math.floor((t / (1000 * 60 * 60 * 24)));
+
+        return {
+            'total' : t,
+            'hours' : hours,
+            'minutes' : minutes,
+            'seconds' : seconds
+        };
+    }
+
+    function setClock (id, endtime) {
+        let timer = document.getElementById(id),
+            hours = timer.querySelector(".hours"),
+            minutes = timer.querySelector('.minutes'),
+            seconds = timer.querySelector('.seconds'),
+            timeInterval = setInterval(updadeClock, 1000);
+
+
+        function updadeClock () {
+            let t = getTimeRemaining (endtime);
+
+            function addZero (num) {
+                if(num <= 9) {
+                    return '0' + num;
+                } else {
+                    return num;
+                }
+            }
+
+            hours.textContent = addZero(t.hours);
+            minutes.textContent = addZero(t.minutes);
+            seconds.textContent = addZero(t.seconds);
+
+
+            if (t.total <= 0) {
+                clearInterval (timeInterval);
+                hours.textContent = '00';
+                minutes.textContent = '00';
+                seconds.textContent = '00';
+            }
+        }
+
+    };
+
+    setClock('timer', deadline);
+});
